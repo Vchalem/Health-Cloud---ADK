7f231408e0b7a073fb9bb18d4512d835fce55bec
Vchalem <Vchalem@gmail.com>
2019-03-08 11:08:27 -0500

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -2,24 +2,21 @@
 
 sfdx force:org:create -f config/project-scratch-def.json -a HCADK2 --setdefaultusername -d 1
 
+sfdx force:package:install --package 04t1C000000AoPO -w 20 
+
+sfdx force:mdapi:deploy --deploydir mdapi-source/app-config
+
+sfdx force:mdapi:deploy --deploydir mdapi-source/data-config
+
+sfdx force:mdapi:deploy --deploydir mdapi-source/org-config
+
+sfdx force:source:push 
+
+sfdx force:user:permset:assign -n HealthCloudPermissionSetLicense
+sfdx force:user:permset:assign -n HealthCloudAdmin
+
 
 sfdx force:apex:execute -f config/create-demo-data-setup.apex
-
-#sfdx force:package:install --package 04t1C000000AoPO -w 20 
-
-#sfdx force:mdapi:deploy --deploydir mdapi-source/app-config
-
-#sfdx force:mdapi:deploy --deploydir mdapi-source/data-config
-
-#sfdx force:mdapi:deploy --deploydir mdapi-source/org-config
-
-#sfdx force:source:push 
-
-#sfdx force:user:permset:assign -n HealthCloudPermissionSetLicense
-#sfdx force:user:permset:assign -n HealthCloudAdmin
-
-
-#sfdx force:apex:execute -f config/create-demo-data-setup.apex
 #sfdx force:apex:execute -f config/create-demo-data.apex
 
 
