a433b2901343e823be212a2dcdea348c7cf1349a
faysdev <faysdev@gmail.com>
2019-03-27 20:06:42 -0400

    Changes to make Contact load succeed.

--- dataInit.sh
+++ dataInit.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # Execute the plan to load the first batch of data items
-sfdx force:data:tree:import --plan data/Plan1.json
+sfdx force:data:tree:import -u HCADK2 -p data/Plan1.json
 
-# Install the plugin to handle RecordType Ids
+# Install the plugin to handle RecordType Ids, in case it wasn't already installed.
 sfdx plugins:install sfdx-wry-plugin
+
+# Remove the post-processing output directory, in case there was one left from earlier run
+rm -rf data/preprocess.out
 
 # Use the plug-in to convert the files with DeveloperName to SF ID's for this org
 sfdx wry:file:replace -u HCADK2 -i data/preprocess
