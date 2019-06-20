4ae50386a4cffab83d473a2c2d6dbc0f1134893e
faysdev <faysdev@gmail.com>
2019-06-20 12:23:51 -0400

    Commented out the data-config metadata and added the -f option to source:push

--- orgInit.sh
+++ orgInit.sh
@@ -5,12 +5,20 @@ sfdx force:org:create -f config/project-scratch-def.json -a HCADK --setdefaultus
 
 # Install the package
 sfdx force:package:install --package 04t1C000000Apj5 -w 20 
+<<<<<<< HEAD
+# 216: --package 04t1C000000AoPO
+# 218: --package 04t1C000000ApHp
+# 220: --package 04t1C000000Apj5
+=======
+>>>>>>> 0f507cf2cea4da76c9200015623a853c33809733
 
-# Deploy the code
+# Deploy the metadata packages
 sfdx force:mdapi:deploy --deploydir mdapi-source/app-config
-sfdx force:mdapi:deploy --deploydir mdapi-source/data-config
+# sfdx force:mdapi:deploy --deploydir mdapi-source/data-config
 sfdx force:mdapi:deploy --deploydir mdapi-source/org-config
-sfdx force:source:push -f
+
+# Deploy the source code (will only work in scratch orgs)
+sfdx force:source:push 
 
 # Assign the permissions
 sfdx force:user:permset:assign -n HealthCloudPermissionSetLicense
