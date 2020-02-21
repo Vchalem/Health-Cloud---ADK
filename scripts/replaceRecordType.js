d779e40111d9001b0cd8645f2d6b257fff2f9767
Ronit Nuguru <ronit.nuguru@gmail.com>
2020-02-21 11:35:16 -0800

    remove task from plan.json

--- scripts/replaceRecordType.js
+++ scripts/replaceRecordType.js
@@ -1,20 +1,25 @@
 var fs = require("fs");
 
-const object = process.argv.slice(2);
-let mapRecordTypes = new Map();
+try {
+  const object = process.argv.slice(2);
+  let mapRecordTypes = new Map();
+  
+  var recTypes = require(`../data/preprocess/${object}RecordTypes.json`);
+  var objectData = require(`../data/preprocess/${object}.json`);
+  
+  recTypes.result.records.forEach(record => {
+    mapRecordTypes.set(record.DeveloperName, record.Id);
+  });
+  
+  objectData.records.forEach(record => {
+      if (mapRecordTypes.has(record.RecordTypeId)) {
+        record.RecordTypeId = mapRecordTypes.get(record.RecordTypeId);
+      }
+  });
+} catch (err) {
+  console.log(err);
+}
 
-var recTypes = require(`../data/preprocess/${object}RecordTypes.json`);
-var objectData = require(`../data/preprocess/${object}.json`);
-
-recTypes.result.records.forEach(record => {
-  mapRecordTypes.set(record.DeveloperName, record.Id);
-});
-
-objectData.records.forEach(record => {
-    if (mapRecordTypes.has(record.RecordTypeId)) {
-      record.RecordTypeId = mapRecordTypes.get(record.RecordTypeId);
-    }
-});
 
 try {
   fs.writeFileSync(`./data/${object}.json`, JSON.stringify(objectData, null, 2));
